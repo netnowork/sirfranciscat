44ebd0a5322bff5eb4549189800e28d02c098663
Paul Jarvis <paul.keith.jarvis@gmail.com>
2022-11-17 11:42:17 -0500

    Navbar toggle is now to the right

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,6 @@
-<<<<<<< HEAD
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $(".navbar-fixed-top");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-});
-=======
-// $(window).scroll(function () {
-//     if ($(document).scrollTop() > 50) {
-//         $('.nav').addClass('affix');
-//         console.log("OK");
-//     } else {
-//         $('.nav').removeClass('affix');
-//     }
+// $(function () {
+//     $(document).scroll(function () {
+//         var $nav = $(".navbar-fixed-top");
+//         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//     });
 // });
-
-
-$(window).scroll(function () {
-
-    if ($(this).scrollTop() > 50) {
-        $('nav').stop().fadeIn(800);
-        console.log("OK");
-    } else {
-        $('nav').stop().fadeOut(800);
-        console.log("OK");
-    };
-}); 
->>>>>>> navbar-one-ul

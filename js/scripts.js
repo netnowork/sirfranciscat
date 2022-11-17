1365e19a691ca91e896fd3b6b3840abf4213f7fc
Paul Jarvis <paul.keith.jarvis@gmail.com>
2022-11-17 17:24:59 -0500

    Everything positioned good. Navbar fade in next

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,35 @@
 //         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 //     });
 // });
+
+// $(function () {
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 20) {
+//             $('nav').fadeOut(400);
+//         } else {
+//             $('nav').fadeIn(400);
+//         };
+//     });
+// });
+
+// $(function() {
+//     let lastKnownScrollPosition = 0;
+//     let ticking = false;
+
+//     function doSomething(scrollPos) {
+//   // Do something with the scroll position
+//     }
+
+//     document.addEventListener('scroll', (e) => {
+//   lastKnownScrollPosition = window.scrollY;
+
+//   if (!ticking) {
+//     window.requestAnimationFrame(() => {
+//       doSomething(lastKnownScrollPosition);
+//       ticking = false;
+//     });
+
+//     ticking = true;
+//   }
+// });
+// });

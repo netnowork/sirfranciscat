ec24d41bab219cf2d5c00f1769480fed2de3c81d
Paul Jarvis <paul.keith.jarvis@gmail.com>
2022-11-19 09:06:21 -0500

    Cards and footer next 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,29 @@
-//last change
 $(function () {
     var DlastKnownScrollPosition = 0;
     var ticking = false;
     var scrollTop = window.pageYoffset || document.documentElement.scrollTop;
-    var titletextel = document.getElementById("testTitle");
     console.log(scrollTop);
-    $('.testTitle').addClass("nav-update-text");
+    $('#testTitle').addClass("nav-update-text");
     if (scrollTop > 0) {
         $('.navbar').addClass("nav-update");
-        $('.testTitle').addClass("fadeInC-text");
+        $('#testTitle').addClass("fadeInC-text");
     } else {
         $('.navbar').addClass("fadeInC");
-        $('.testTitle').addClass("nav-update-text");
+        $('#testTitle').addClass("nav-update-text");
     }
 
     function navFade(scrollPos) {
         if (scrollPos > 0) {
             $('.navbar').removeClass("fadeInC");
-            $('.testTitle').removeClass("nav-update-text");
+            $('#testTitle').removeClass("nav-update-text");
             $('.navbar').addClass("nav-update");
-            $(".testTitle").addClass("fadeInC-text");
-
-            console.log("Should be Black");
+            $("#testTitle").addClass("fadeInC-text");
         } else {
             $('.navbar').removeClass("nav-update");
-            $('.testTitle').removeClass("fadeInC-text");
+            $('#testTitle').removeClass("fadeInC-text");
             $('.navbar').addClass("fadeInC");
-            $(".testTitle").addClass("nav-update-text");
+            $("#testTitle").addClass("nav-update-text");
         }
-        console.log("Should be Transparent");
     }
 
 
